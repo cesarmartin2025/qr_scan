faab78b2263053c153a8fcdc32eb6baed823838c
cesar <cesarmartin@paucasesnovescifp.cat>
2026-02-21 09:58:09 +0100

    He impliementado la funcionalidad de poder crear marcadores  tmeporales clickando en la pantalla

--- lib/screens/mapa_screen.dart
+++ lib/screens/mapa_screen.dart
@@ -15,9 +15,10 @@ class _MapaScreenState extends State<MapaScreen> {
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
   MapType currentMapType = MapType.normal;
+
+  Set<Marker> markers = <Marker>{};
   @override
   Widget build(BuildContext context) {
-    
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
 
@@ -25,12 +26,16 @@ class _MapaScreenState extends State<MapaScreen> {
       target: scan.getLatLng(),
       zoom: 17,
     );
-
-    Set<Marker> markers = <Marker>{};
-    markers.add(Marker(
-      markerId: const MarkerId('localizacion'),
-      position: scan.getLatLng(),
-    ));
+    if (markers.isEmpty) {
+      markers.add(Marker(
+        markerId: const MarkerId('localizacion-inicial'),
+        position: scan.getLatLng(),
+        infoWindow: InfoWindow(
+          title: 'Localización guardada',
+          snippet:  'Lat : ${scan.getLatLng().latitude} Long : ${scan.getLatLng().longitude}',
+        )
+      ));
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -40,46 +45,69 @@ class _MapaScreenState extends State<MapaScreen> {
             onPressed: () async {
               final GoogleMapController controller = await _controller.future;
 
-              controller.animateCamera(CameraUpdate.newCameraPosition(
-                  CameraPosition(
-                    target: scan.getLatLng(),
-                    zoom: 17,
-                    tilt: 50,
-                  )));
+              controller
+                  .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: scan.getLatLng(),
+                zoom: 17,
+                tilt: 50,
+              )));
             },
-          )
+          ),
+          IconButton(
+            tooltip: 'Borrar todos los marcadores',
+            onPressed: () {
+
+            setState(() {
+              markers.clear();
+            });
+
+          }, icon: const Icon(Icons.delete_sweep))
+          
         ],
       ),
       body: GoogleMap(
+        myLocationEnabled: true,
         mapType: currentMapType,
         markers: markers,
         initialCameraPosition: _puntoInicial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
+        onTap: (LatLng posicionClickada) => setState(() {
+          markers.add(Marker(
+            markerId: MarkerId('localizacion : ${posicionClickada.toString()}'),
+            position: posicionClickada,
+            infoWindow: InfoWindow(
+                title: 'Ubicación seleccionada',
+                snippet:
+                    'Lat : ${posicionClickada.latitude} Long : ${posicionClickada.longitude}',
+                onTap: () {
+
+                },),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          ));
+          print('Tocaste');
+        }),
       ),
-      floatingActionButton:
-       Padding(
-         padding: const EdgeInsets.only(right: 0.0,bottom: 5.0),
-         child: FloatingActionButton(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(right: 0.0, bottom: 5.0),
+        child: FloatingActionButton(
           child: const Icon(Icons.layers),
           backgroundColor: Colors.white.withValues(alpha: 0.7),
-          foregroundColor: Colors.black,         
-          onPressed: (){
+          foregroundColor: Colors.black,
+          onPressed: () {
             setState(() {
-              if(currentMapType==MapType.normal){
+              if (currentMapType == MapType.normal) {
                 currentMapType = MapType.hybrid;
-              } else{
+              } else {
                 currentMapType = MapType.normal;
               }
             });
           },
-          
-          ),
-          
-       ),
-       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
   }
 }
