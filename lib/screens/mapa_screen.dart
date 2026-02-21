083ea5e14ac33e8865e8a65da3fd858b1cf34abc
cesar <cesarmartin@paucasesnovescifp.cat>
2026-02-21 10:39:52 +0100

    Implementación de un botón que cambie la inclinación del mapa.

--- lib/screens/mapa_screen.dart
+++ lib/screens/mapa_screen.dart
@@ -17,6 +17,8 @@ class _MapaScreenState extends State<MapaScreen> {
   MapType currentMapType = MapType.normal;
 
   Set<Marker> markers = <Marker>{};
+
+  double currentTilt = 0.0;
   @override
   Widget build(BuildContext context) {
     final ScanModel scan =
@@ -28,19 +30,20 @@ class _MapaScreenState extends State<MapaScreen> {
     );
     if (markers.isEmpty) {
       markers.add(Marker(
-        markerId: const MarkerId('localizacion-inicial'),
-        position: scan.getLatLng(),
-        infoWindow: InfoWindow(
-          title: 'Localización guardada',
-          snippet:  'Lat : ${scan.getLatLng().latitude} Long : ${scan.getLatLng().longitude}',
-        )
-      ));
+          markerId: const MarkerId('localizacion-inicial'),
+          position: scan.getLatLng(),
+          infoWindow: InfoWindow(
+            title: 'Localización guardada',
+            snippet:
+                'Lat : ${scan.getLatLng().latitude} Long : ${scan.getLatLng().longitude}',
+          )));
     }
 
     return Scaffold(
       appBar: AppBar(
         actions: [
           IconButton(
+            tooltip: 'Volver a la localizacion inicial',
             icon: const Icon(Icons.location_searching),
             onPressed: () async {
               final GoogleMapController controller = await _controller.future;
@@ -54,15 +57,39 @@ class _MapaScreenState extends State<MapaScreen> {
             },
           ),
           IconButton(
-            tooltip: 'Borrar todos los marcadores',
-            onPressed: () {
+              tooltip: 'Borrar todos los marcadores',
+              onPressed: () {
+                setState(() {
+                  markers.clear();
+                });
+              },
+              icon: const Icon(Icons.delete_sweep)),
+          IconButton(
+              tooltip: 'Cambiar inclinacion',
+              onPressed: () async {
+                if (currentTilt == 0.0) {
+                  currentTilt = 45.0;
+                } else if (currentTilt == 45.0) {
+                  currentTilt = 75.0;
+                } else {
+                  currentTilt = 0.0;
+                }
+                // 4. Obtenemos el controlador y aplicamos la inclinación
+                final GoogleMapController controller = await _controller.future;
 
-            setState(() {
-              markers.clear();
-            });
+                // Obtenemos la posición actual para no perder el centro ni el zoom
+                // Nota: Si usas la posición del scan, siempre volverá al centro del QR
+                controller.animateCamera(
+                    CameraUpdate.newCameraPosition(CameraPosition(
+                  target: scan
+                      .getLatLng(), // O puedes obtener la posición actual del mapa
+                  tilt: currentTilt,
+                  zoom: 17,
+                )));
 
-          }, icon: const Icon(Icons.delete_sweep))
-          
+                setState(() {});
+              },
+              icon: const Icon(Icons.threed_rotation))
         ],
       ),
       body: GoogleMap(
@@ -78,16 +105,14 @@ class _MapaScreenState extends State<MapaScreen> {
             markerId: MarkerId('localizacion : ${posicionClickada.toString()}'),
             position: posicionClickada,
             infoWindow: InfoWindow(
-                title: 'Ubicación seleccionada',
-                snippet:
-                    'Lat : ${posicionClickada.latitude} Long : ${posicionClickada.longitude}',
-                onTap: () {
-
-                },),
+              title: 'Ubicación seleccionada',
+              snippet:
+                  'Lat : ${posicionClickada.latitude} Long : ${posicionClickada.longitude}',
+              onTap: () {},
+            ),
             icon:
                 BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
           ));
-          print('Tocaste');
         }),
       ),
       floatingActionButton: Padding(
